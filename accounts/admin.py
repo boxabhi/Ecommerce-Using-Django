1ead856aede9d2d600ded7083e80819d8d9b3b25
abhijeet gupta <abhijeetg40@gmail.com>
2020-05-11 21:30:43 +0530

    removed one app

--- accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Vendor,Cart
-# Register your models here.
-
-admin.site.register(Vendor)
-admin.site.register(Cart)
--- city/urls.py
+++ city/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
-from .views import home,cart,buynow
 
+from .views import register,login,vendorinfo,error,logout_view,add_cart,remove,home,cart,buynow
 
+from dashboard.views import dashboard
 
 urlpatterns = [    
     path('' , home , name="home"),
     path('cart', cart , name="cart"),
-    path('buynow',buynow , name="buynow")
+    path('buynow',buynow , name="buynow"),
+    
+     path('register' , register , name="register" ),
+    path('login' , login , name="login"),
+    path('vendor-info' , vendorinfo , name="vendor-info"),
+    path('404' , error , name="404"),
+    path('dashboard' , dashboard , name="dashboard-home"),
+    path('logout' , logout_view , name="logout"),
+    
+    path('add-cart/<id>' , add_cart , name="add-cart"),
+    path('remove/<id>' , remove , name="remove"),
 
 ]
